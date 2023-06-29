b60a9b8ecac82b9e5badfba5c04616a2f6187aa1
Dhruvp132 <dhruvpatel1321@gmail.com>
2023-06-30 01:00:52 +0530

    second

---
+++ shop/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request) :
+    return render(request, 'shop/index.html')
+
+def about(request) :
+    return HttpResponse("We are at about")
+
+def tracker(request) :
+    return HttpResponse("We are at tracker")
+
+def contact(request) :
+    return HttpResponse("We are at contact")
+
+def search(request) :
+    return HttpResponse("We are at search")
+
+def productView(request) :
+    return render(request, 'shop/index.html')
+
+def checkout(request) :
+    return HttpResponse("We are at checkout")
+
